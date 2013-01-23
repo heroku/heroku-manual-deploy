5344ba7fe2af3d7535cf366c82f05ac20afc2cfd
Mark Fine <mark.fine@gmail.com>
2013-01-23 14:49:49 -0800

    interval in rolling.

--- lib/heroku/deploy.rb
+++ lib/heroku/deploy.rb
@@ -2,6 +2,23 @@ require "heroku/command/base"
 
 class Heroku::Command::Deploy < Heroku::Command::Base
 
+  # deploy [PROCESS]
+  #
+  # deploy an app
+  #
+  # if PROCESS is not specified, deploy all processes on the app
+  #
+  #Examples:
+  #
+  # $ heroku deploy web.1
+  # Deploying web.1 process... done
+  #
+  # $ heroku deploy:rolling web
+  # Deploying web processes... done
+  #
+  # $ heroku deploy:rolling
+  # Deploying processes... done
+  #
   def index
     process = shift_argument
     validate_arguments!
@@ -22,34 +39,65 @@ class Heroku::Command::Deploy < Heroku::Command::Base
     end
   end
 
+  # deploy:rolling [PROCESS]
+  #
+  # rolling deploy of an app
+  #
+  # if PROCESS is not specified, deploy all processes on the app
+  # -i, --interval SECONDS  # time between deploys
+  #
+  #Examples:
+  #
+  # $ heroku deploy:rolling web.1
+  # Deploying web.1 process... done
+  #
+  # $ heroku deploy:rolling web
+  # Deploying web processes...
+  # Deploying web.1 process... done
+  # done
+  #
+  # $ heroku deploy:rolling
+  # Deploying processes...
+  # Deploying web.1 process... done
+  # done
+  #
   def rolling
     process = shift_argument
     validate_arguments!
+    interval = options[:interval]
 
     message, options = case process
     when NilClass
-      action("Deploying processes\n") do
-        api.get_ps(app).body.each do |entry|
+      action("Deploying processes") do
+        first = true
+        entries = api.get_ps(app).body
+        entries.each_with_index do |entry, index|
           ps = entry['process']
-          action("Deploying #{ps} process\n") do
+          action((first ? "\n" : "") + "Deploying #{ps} process") do
             api.post_ps_restart(app, { :ps => ps })
           end
+          first = false
+          sleep interval.to_i if interval && index+1 != entries.size
         end
       end
     when /.+\..+/
       ps = args.first
-      action("Deploying #{ps} process\n") do
+      action("Deploying #{ps} process") do
         api.post_ps_restart(app, { :ps => ps })
       end
     else
       type = args.first
-      action("Deploying #{type} process\n") do
-        api.get_ps(app).body.each do |entry|
+        action("Deploying #{type} process") do
+        first = true
+        entries = api.get_ps(app).body
+        entries.each_with_index do |entry, index|
           ps = entry['process']
           if ps.split(".").first == type
-            action("Deploying #{ps} process\n") do
+            action((first ? "\n" : "") + "Deploying #{ps} process") do
               api.post_ps_restart(app, { :ps => ps })
             end
+            first = false
+            sleep interval.to_i if interval && index+1 != entries.size
           end
         end
       end
